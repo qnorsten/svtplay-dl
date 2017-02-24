c59628f40eb4d0f60268d55ac2b056da7f3e9afc
qnorsten <qnorsten@hotmail.com>
2017-02-24 10:01:11 +0100

    Info: Now takes data as a dict and handles formating

--- lib/svtplay_dl/info/__init__.py
+++ lib/svtplay_dl/info/__init__.py
@@ -10,13 +10,43 @@ class info(object):
         self.options = options
         self.subfix = subfix
 
-    def save(self):
+    def save_info(self):
         
         if self.subfix:
             self.options.output = self.options.output + self.subfix
+        #Might add support for other types here later    
+        data = self.raw_txt(self.data)
+        self.save_file(data, "txt")
+    
+    def raw_txt(self, data): 
+        text = ''
+        if "show" in data:
+            text += "Show: %s" % (data['show'])
+        if "title" in data:  
+            text += "\nTitle: %s" % data['title']
+        if "broadcastDate" in data:
+            text+= "\nBroadcast Date: %s" % data["broadcastDate"]
+        if "publishDate" in data:
+            text+= "\nPublishDate Date: %s" % data['publishDate']
+        if "duration" in data:
+            text+= "\nDuration: %s min" % data['duration']
+        if "season" in data:
+            text+= "\nSeason: %s" % data['season']
+        if "episode" in data:
+            text+= "\nEpisode: %s" % data['episode']
+        if "type" in data:
+            text+="\nType: %s" % data['type']
             
-        self.save_file(self.data, "txt")
-        
+        if "audiodescription" in data:
+            text+="Audiodescription: True"
+        if "signInterpretation" in data:
+            text+="Sign Interpretation: True"
+        if "subtitle" in data:
+            text+="\nSubtitled: True"
+        if "description" in data:
+            text+= "\nDescription: " + data["description"]
+        return text
+    
     def save_file(self, data, subtype):
         if platform.system() == "Windows" and is_py3:
             file_d = output(self.options, subtype, mode="wt", encoding="utf-8")
