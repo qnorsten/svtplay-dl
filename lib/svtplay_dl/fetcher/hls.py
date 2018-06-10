da181663e61309ee78fa6f833294d6c2010fd94c
Unknown <qnorsten@hotmail.com>
2018-06-10 16:31:04 +0200

    fix for missing dplay-subtitles

--- lib/svtplay_dl/fetcher/hls.py
+++ lib/svtplay_dl/fetcher/hls.py
@@ -67,13 +67,14 @@ def hlsparse(config, res, url, **kwargs):
             audio_url = None
             if i["TAG"] == "EXT-X-MEDIA":
                 if "AUTOSELECT" in i and (i["AUTOSELECT"].upper() == "YES"):
-                    if i["TYPE"] and i["TYPE"] != "SUBTITLES":
+                    if i["TYPE"]:
                         if "URI" in i:
-                            if segments is None:
-                                segments = True
                             if i["GROUP-ID"] not in media:
                                 media[i["GROUP-ID"]] = []
                             media[i["GROUP-ID"]].append(i["URI"])
+                            if i["TYPE"] != "SUBTITLES":  # perhaps better to check if i["TYPE"] == "AUDIO": ?
+                                if segments is None:
+                                    segments = True
                         else:
                             segments = False
                 if i["TYPE"] == "SUBTITLES":
